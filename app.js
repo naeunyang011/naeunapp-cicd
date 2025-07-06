2609100b31f46e276d9931af81b5c34aa3fbfba8
Ubuntu <ubuntu@ip-172-31-0-138.ap-southeast-2.compute.internal>
2025-07-06 13:24:34 +0000

    first commit - add app.js

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+  host: 'naeundbinstance.c7kokc40medh.ap-southeast-2.rds.amazonaws.com',
+  user: 'admin',
+  password: 'naeunnaeun01!B',
+  database: 'myapp'
+});
+
+const server = http.createServer((req, res) => {
+    connection.query('SELECT * FROM test', (err, results) => {
+        if (err) {
+            res.writeHead(500);
+            res.end('Database Error');
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(results));
+    });
+});
+
+server.listen(3000, '0.0.0.0', () => {
+    console.log('Server running on port 3000');
+});
